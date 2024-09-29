f07ef593cd405a03a9c27ae2c70369f0d8877b78
MateoGoitea <goiteamateonicolas73@gmail.com>
2024-09-29 16:44:19 -0300

    Sprites jugador
    Se añadieron sprites al jugador que cambian dependiendo la direccion en la que se mueve

--- juego/src/scenes/Play.js
+++ juego/src/scenes/Play.js
@@ -9,13 +9,13 @@ class Play extends Phaser.Scene{
 
     preload(){
         this.load.image('cielo', '../juego/public/resources/img/cielo.jpg');
-        this.load.image('nave', '../juego/public/resources/img/naveespacial.png');
+        this.load.spritesheet('nave', '../juego/public/resources/img/spritenave.png', {frameWidth:50 ,frameHeight:46});
         this.load.image('meteoro', '../juego/public/resources/img/meteoro.png');
     }
 
     create(){
         this.add.image(400,300,'cielo');
-        this.jugador = this.physics.add.sprite(400,550, 'nave');
+        this.jugador = this.physics.add.sprite(400,550, 'nave',1);
         this.jugador.setCollideWorldBounds(true);
 
         this.grupoMeteoros = this.physics.add.group();
@@ -26,6 +26,23 @@ class Play extends Phaser.Scene{
         this.physics.add.collider(this.jugador, this.grupoMeteoros, this.gameOver, null, this);
 
         this.textoDePuntaje = this.add.text(16,16,'Puntaje: 0', {fontSize:'32px', fill:'#fff'});
+
+
+        this.anims.create({
+            key:'izquierda',
+            frames: [{key:'nave', frame:0}],
+            frameRate: 20
+        });
+        this.anims.create({
+            key:'normal',
+            frames: [{key:'nave', frame:1}],
+            frameRate: 20
+        });
+        this.anims.create({
+            key:'derecha',
+            frames: [{key:'nave', frame:2}],
+            frameRate: 20
+        });
     }
 
     generarMeteoros() {
@@ -44,12 +61,15 @@ class Play extends Phaser.Scene{
             
     update(){
         this.jugador.setVelocityX(0);
-        
+        this.jugador.anims.play('normal', true);
         
         if (this.cursors.left.isDown) {
         this.jugador.setVelocityX(-300);
-        } else if (this.cursors.right.isDown) {
+        this.jugador.anims.play('izquierda', true);
+        }
+        else if (this.cursors.right.isDown) {
         this.jugador.setVelocityX(300);
+        this.jugador.anims.play('derecha', true);
         }
 
         this.puntaje += 1;
